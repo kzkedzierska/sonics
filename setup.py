70c25932e6e6baa63278a47ff2a03cab44d87f33
kzkedzierska <kzkedzierska@gmail.com>
2020-03-10 13:20:36 +0000

    towards pip install

--- setup.py
+++ setup.py
@@ -4,5 +4,16 @@ import numpy
 
 setup(
         ext_modules=cythonize("sonics.pyx"),
-        include_dirs=[numpy.get_include()]
+        include_dirs=[numpy.get_include()],
+        name="sonics",
+        version="0.2.0",
+        description=description
 )
+
+description = """
+SONiCS performs dense forward simulations of the PCR of Short Tandem Repeats
+from capture experiments, calculates the likelihood of generating the provided
+read support (reads per allele) out of the final PCR pool and determines
+the most probable genotype based on the log likelihood distributions
+from all such simulations.
+"""
